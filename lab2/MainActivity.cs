0fe5099f230dbe8ce01bbbb6914e1344667423f5
Rigotka <sergey_sorokin.1580@mail.ru>
2024-04-09 01:33:46 +0700

    добавил верстку

--- lab2/MainActivity.cs
+++ lab2/MainActivity.cs
@@ -3,51 +3,48 @@ using Android.App;
 using Android.OS;
 using Android.Runtime;
 using Android.Views;
-using AndroidX.AppCompat.Widget;
 using AndroidX.AppCompat.App;
-using Google.Android.Material.FloatingActionButton;
-using Google.Android.Material.Snackbar;
+using Android.Widget;
 
 namespace lab2
 {
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
+        private LinearLayout _container;
+
+        private int _viewCont = 0;
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             SetContentView(Resource.Layout.activity_main);
 
-            Toolbar toolbar = FindViewById<Toolbar>(Resource.Id.toolbar);
-            SetSupportActionBar(toolbar);
+            var button = FindViewById<Button>(Resource.Id.button);
+            button.Click += Button_Click;
 
-            FloatingActionButton fab = FindViewById<FloatingActionButton>(Resource.Id.fab);
-            fab.Click += FabOnClick;
+            _container = FindViewById<LinearLayout>(Resource.Id.container);
         }
 
-        public override bool OnCreateOptionsMenu(IMenu menu)
+        private void Button_Click(object sender, EventArgs e)
+        { 
+            AddTextView();
+        }
+
+        private void AddTextView()
+        {
+            TextView textView = new TextView(this);
+            textView.SetText(_viewCont.ToString(), TextView.BufferType.Normal);
+            _viewCont++;
+            _container.AddView(textView);
+
+        }
+
+    public override bool OnCreateOptionsMenu(IMenu menu)
         {
             MenuInflater.Inflate(Resource.Menu.menu_main, menu);
             return true;
-        }
-
-        public override bool OnOptionsItemSelected(IMenuItem item)
-        {
-            int id = item.ItemId;
-            if (id == Resource.Id.action_settings)
-            {
-                return true;
-            }
-
-            return base.OnOptionsItemSelected(item);
-        }
-
-        private void FabOnClick(object sender, EventArgs eventArgs)
-        {
-            View view = (View) sender;
-            Snackbar.Make(view, "Replace with your own action", Snackbar.LengthLong)
-                .SetAction("Action", (View.IOnClickListener)null).Show();
         }
 
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
@@ -56,6 +53,6 @@ namespace lab2
 
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
         }
-	}
+    }
 }
 
